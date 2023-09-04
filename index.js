e65c7f6192806f1d66c61e4f93733846570d1a4c
Sharku-Kun <shvrku@gmail.com>
2023-09-04 23:20:45 +0630

    Cors Enabled + Value Changes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors'); // Import the cors middleware
 const app = express();
+
+// Use cors middleware to enable CORS for all routes
+app.use(cors());
 
 app.get('/', async function(req, res) {
   try {
@@ -20,8 +24,11 @@ app.get('/', async function(req, res) {
     const HSPercentage = $('.giant-stats > :nth-child(3) > .wrapper > .numbers > .value').text();
 
     const WinRate = $('.giant-stats > :nth-child(4) > .wrapper > .numbers > .value').text()
+
+    const KDRatio = $('.giant-stats > :nth-child(2) > .wrapper > .numbers > .value').text()
+    
     // Create a JSON response with the desired structure
-    const json = { "Rank": Rank, "HS%": HSPercentage, "Win %": WinRate };
+    const json = { "Rank": Rank, "HS%": HSPercentage, "Win %": WinRate, "K/D": KDRatio };
 
     // Send the JSON as a response to the client
     res.send(json);
