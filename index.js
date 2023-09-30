ec2068dd33f88edae329c691f5fb5b258a6b86b8
Sharku <108813159+shvrku@users.noreply.github.com>
2023-09-30 14:25:30 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const u = 'https://api.sharkyfb.repl.co'; 
-fetch(u)
-.then(response => response.text())
-.then(data => {
+const data = No;
   if (data === 'No') {
 
     document.body.innerHTML = '<h1>Access Denied</h1><p>Sorry, the content is not available.</p>';
   }
-})
+
